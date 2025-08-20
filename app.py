3387993424f929103cccba870c92b3b83fefd856
ferifirmansah05 <100080090+ferifirmansah05@users.noreply.github.com>
2025-08-20 16:18:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ app = dash.Dash(__name__)
 server = app.server
 
 app.layout = html.Div([
-    html.H1("Visualisasi Sunburst),
+    html.H1("Visualisasi Sunburst"),
     
     html.Label("Pilih Bulan:"),
     dcc.Dropdown(
